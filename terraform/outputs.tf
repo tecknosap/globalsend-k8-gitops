4cf1981343732c9a45209ff8a4aff1c86cbb21da
Mahad Mohamed <tecknosap@gmail.com>
2026-02-24 18:36:31 +0000

    Initial commit

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,39 +1,26 @@
-########################
-# Outputs for Prometheus access
-# - NodePort number
-# - Full URL for localhost access
-########################
+# Prometheus outputs: NodePort and localhost URL
 output "prometheus_nodeport" {
   value       = data.kubernetes_service.prometheus.spec[0].port[0].node_port
-  description = "NodePort for accessing Prometheus"
+  description = "NodePort for Prometheus"
 }
 
 output "prometheus_url" {
   value       = "http://localhost:${data.kubernetes_service.prometheus.spec[0].port[0].node_port}"
-  description = "URL to access Prometheus"
+  description = "Prometheus access URL"
 }
 
-########################
-# Outputs for Grafana access
-# - NodePort number
-# - Full URL for localhost access
-########################
+# Grafana outputs: NodePort and localhost URL
 output "grafana_nodeport" {
   value       = data.kubernetes_service.grafana.spec[0].port[0].node_port
-  description = "NodePort for accessing Grafana"
+  description = "NodePort for Grafana"
 }
 
 output "grafana_url" {
   value       = "http://localhost:${data.kubernetes_service.grafana.spec[0].port[0].node_port}"
-  description = "URL to access Grafana"
+  description = "Grafana access URL"
 }
 
-
-########################
-# Outputs for Argo CD access
-# - Admin password (nonsensitive to avoid marking as secret)
-# - Example kubectl port-forward command to access the UI
-########################
+# Argo CD outputs: admin password and port-forward command
 output "argocd_admin_password" {
   value       = nonsensitive(data.kubernetes_secret.argocd_initial_password.data["password"])
   description = "Argo CD initial admin password"
